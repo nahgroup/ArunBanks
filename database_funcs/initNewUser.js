16ae3c6e047ad943266fb1808600f09e631a4987
Will R <15824869+Willumz@users.noreply.github.com>
2020-10-25 04:19:19 +0000

    Added registration

---
+++ database_funcs/initNewUser.js
@@ -0,0 +1,14 @@
+const fbadmin = require("firebase-admin");
+
+module.exports = function(userID, name, advanced, callback) {
+
+    var coll = fbadmin.firestore().collection("users");
+
+    coll.doc().set({
+        userID,
+        name,
+        balance: 0,
+        advanced
+    });
+
+}
