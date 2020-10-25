c0f983ae37590b49c94a4eb7de2c18362b3bfa9b
Dan <27007457+DanielJennings2001@users.noreply.github.com>
2020-10-25 01:55:31 +0100

    Cool Stuff

--- Catching Money/Assets/Scripts/JavaImplimentation.cs
+++ Catching Money/Assets/Scripts/JavaImplimentation.cs
@@ -6,24 +6,26 @@ using System;
 
 public class JavaImplimentation : MonoBehaviour
 {
-    /*[DllImport("__Internal")]
-    private static extern void Complete();
-
     [DllImport("__Internal")]
-    private static extern double TheInput();
+    private static extern void Complete();
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Score").GetComponent<FallingManager>().DepositAmount = TheInput();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(gameObject.GetComponent<SpriteRenderer>().color.a == 1)
+        if (gameObject.GetComponent<SpriteRenderer>().color.a == 1)
         {
             Complete();
         }
-    }*/
+    }
+
+    void GetDepositValue(string Deposit)
+    {
+        double Depo = double.Parse(Deposit);
+        GameObject.Find("Score").GetComponent<FallingManager>().DepositAmount = Depo;
+    }
 }
